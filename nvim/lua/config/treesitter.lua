af4b727bfb7c83b3dfb4750082dba1a3edeb640d
jackpot-nfer <jpotrykus@nference.net>
2022-04-12 20:31:53 -0500

    updated treesitter config: install maintained -> all

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained', -- One of 'all', 'maintained' (parsers with maintainers), or a list of languages
+  ensure_installed = 'all', -- One of 'all', 'maintained' (parsers with maintainers), or a list of languages
   highlight = {enable = true}, -- enable = false will disable the whole extension
   indent = {enable = true} -- Allow treesitter to set the indents
 }
